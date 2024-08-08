dce2bdcbf604b14e3af0ed7c211b3b0d6b723dd7
Malte Münch <muench@b1-systems.de>
2024-08-08 10:03:57 +0200

    Remove docs folder (pages are now deployed from gh-pages) (#2)
    * Remove docs folder (pages are now deployed from gh-pages)
    * Add hint that pages is deployed from gh-pages branch in README
    * Alter cronjob to use gh-pages branch to push aggregated results
    * Move cronjob to 3 o clock
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>

--- cronjob.sh
+++ cronjob.sh
@@ -8,12 +8,16 @@ END_SECONDS=$(date -d "$TODAY" +%s)
 
 GARDENLINUX_VERSION=$(( (END_SECONDS - START_SECONDS) / 86400 ))
 
+OUTFILE=$(mktemp)
+
 cd package-aggregator || exit 1
-go run . -o ../docs/packages/$GARDENLINUX_VERSION.json -exclude package-build
+go run . -o $OUTFILE -exclude package-build
 cd ..|| exit 2
 
 git config --global user.email "package_aggregator@gardenlinux.io"
 git config --global user.name "package_aggregator"
-git add docs/packages/$GARDENLINUX_VERSION.json
+git checkout gh-pages
+cp $OUTFILE packages/$GARDENLINUX_VERSION.json
+git add packages/$GARDENLINUX_VERSION.json
 git commit -m "Package status for Gardenlinux Day $GARDENLINUX_VERSION"
-git push -u origin main
+git push origin gh-pages
