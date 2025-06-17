36269e55fcbe5cacc7889cf4aa773a0c7e43bdff
Eike Waldt <waldt@b1-systems.de>
2025-06-17 10:16:29 +0200

    move packages to packages branch

--- cronjob.sh
+++ cronjob.sh
@@ -10,16 +10,19 @@ END_SECONDS=$(date -d "$TODAY" +%s)
 
 GARDENLINUX_VERSION=$(((END_SECONDS - START_SECONDS) / 86400))
 
-OUTFILE=$(mktemp)
+git config --global user.email "package_aggregator@gardenlinux.io"
+git config --global user.name "package_aggregator"
 
+OUTFILE=$(mktemp)
 cd package-aggregator || exit 1
 go run . -o $OUTFILE -exclude package-build,package-python3.11
 cd .. || exit 2
 
-git config --global user.email "package_aggregator@gardenlinux.io"
-git config --global user.name "package_aggregator"
+git fetch origin
+git checkout packages
+# git reset --hard origin/packages
+
 cp $OUTFILE packages/$GARDENLINUX_VERSION.json
-git fetch origin gh-pages
 git add packages/$GARDENLINUX_VERSION.json
 git commit -m "Package status for Gardenlinux Day $GARDENLINUX_VERSION"
-git push origin gh-pages
+git push origin packages
