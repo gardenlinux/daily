e08aee65a8bf05e0df4999b1972f506d65b05559
Eike Waldt <waldt@b1-systems.de>
2025-06-30 10:49:01 +0200

    checkout origin/packages in package_aggregator

--- cronjob.sh
+++ cronjob.sh
@@ -10,8 +10,8 @@ END_SECONDS=$(date -d "$TODAY" +%s)
 
 GARDENLINUX_VERSION=$(((END_SECONDS - START_SECONDS) / 86400))
 
-git config --global user.email "package_aggregator@gardenlinux.io"
-git config --global user.name "package_aggregator"
+git config user.email "package_aggregator@gardenlinux.io"
+git config user.name "package_aggregator"
 
 OUTFILE=$(mktemp)
 cd package-aggregator || exit 1
@@ -19,8 +19,7 @@ go run . -o $OUTFILE -exclude package-build,package-python3.11
 cd .. || exit 2
 
 git fetch origin
-git checkout packages
-# git reset --hard origin/packages
+git checkout -B packages origin/packages
 
 cp $OUTFILE packages/$GARDENLINUX_VERSION.json
 git add packages/$GARDENLINUX_VERSION.json
